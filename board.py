2a5c7f842a180ff2057e9ee3ee0134f1373e5423
Joshua Jensen <joshjensenjdj2@gmail.com>
2020-04-09 19:08:34 -0600

    created simple board

--- board.py
+++ board.py
@@ -28,7 +28,7 @@ my_w_imgs = {}
 for x in range(12):
     my_w_imgs[x] = ImageTk.PhotoImage(Image.open("nesnej/ludum/output2-onlinepngtools.png"))
 for x in range(12):
-    black_pieces[x] = Button(root, image=my_w_imgs[x])
+    black_pieces[x] = Button(root, bg='pink', image=my_w_imgs[x])
 #print(black_pieces)
 
 #creating the black pieces
@@ -38,14 +38,16 @@ my_b_imgs ={}
 for x in range(12):
     my_b_imgs[x] = ImageTk.PhotoImage(Image.open("nesnej/ludum/output-onlinepngtools.png"))
 for x in range(12):
-    white_p1[x] = Button(root, image=my_b_imgs[x])
+    white_p1[x] = Button(root, bg='purple', image=my_b_imgs[x])
 #print(white_p1)
 
 
 #Placing the black pieces onto the board
 row_c = 0 
 column_c = 1
+black_piece_location = []
 for key, value in black_pieces.items():
+    black_piece_location.append([row_c, column_c])
     value.grid(row=row_c, column=column_c)
     if column_c == 7:
         row_c += 1
@@ -59,7 +61,9 @@ for key, value in black_pieces.items():
 #Placing the white pieces onto the board
 bot_row = 5
 bot_column = 0
+white_piece_location = []
 for key, value in white_p1.items():
+    white_piece_location.append([bot_row, bot_column])
     value.grid(row=bot_row, column=bot_column)
     if bot_column == 6:
         bot_row += 1
@@ -85,7 +89,7 @@ for j in range(0,7,2):
 
 
 #To move location of piece simply use this line below but change the x to the piece you want to move, y the row where you want it, and z the column where you want it
-black_pieces[1].grid(row=3, column=2)
+#black_pieces[1].grid(row=3, column=2)
 
 
 board = []
@@ -93,15 +97,17 @@ rows = []
 
 for x in range(4):
     rows = []
-    for x in range(7):
+    for x in range(8):
         rows.append(redRow[x])
     board.append(rows)
     rows = []
-    for x in range(7):
+    for x in range(8):
         rows.append(blackRow[x])
     board.append(rows)
 
+pieces = []
 
+#for 
 
 """
 game_board = [
@@ -117,4 +123,23 @@ game_board = [
 ]
 """
 
+
+simple_board = []
+for row in board:
+    simple_row = []
+    for square in row:
+        simple_color = square.cget('bg')
+
+        if simple_color == 'black':
+            simple_row.append('o')
+        elif simple_color == 'red':
+            simple_row.append('x')
+    simple_board.append(simple_row)
+#print(simple_board)
+for piece in black_piece_location:
+    simple_board[piece[0]][piece[1]] = '2'
+for piece in white_piece_location:
+    simple_board[piece[0]][piece[1]] = '1'
+
+print(simple_board)
 mainloop()
